d91f509533a5a6125090a293d72cd3edb4147d6a
liaotian1005 <ltz@123456>
2019-12-16 16:40:39 +0800

    commit code.

---
+++ src/org/liaotianzheng/frame/MyBatisType.java
@@ -0,0 +1,13 @@
+/* 
+ * CreateDate 2016-7-18
+ *
+ * Email ：liaotianzheng@aegis-data.cn
+ * School：CUIT 
+ * ltz
+ */
+package org.liaotianzheng.frame;
+/**
+ */
+public enum MyBatisType {
+	dao,mapper
+}
